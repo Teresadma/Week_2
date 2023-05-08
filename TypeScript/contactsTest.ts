195d9f98f3d56e7fbecc62170770ef91f79f5407
Teresa de Miguel <teresa.demiguel.alberti@gmail.com>
2023-05-08 14:07:58 +0200

    repaso 1

--- TypeScript/contactsTest.ts
+++
@@ -1,9 +0,0 @@
-import {Contacts} from "./contacts";
-import {Person} from "./person"
-let jose: Person = new Person("Jose",56,"Calle Almortas");
-let loli: Person = new Person("Loli",12,"Paseo Mallorca");
-let nano:Person = new Person("Nano",43,"Calle Dulcinea");
-
-let agenda = new Contacts();
-agenda.people = [jose,loli,nano]
-console.log(agenda.people);
