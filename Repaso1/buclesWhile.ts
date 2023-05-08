0b49d3e064305e36b9c1d6ad8b55dd8efe0cb881
Teresa de Miguel <teresa.demiguel.alberti@gmail.com>
2023-05-08 09:57:46 +0200

    Subida modificaciones

--- Repaso1/buclesWhile.ts
+++ Repaso1/buclesWhile.ts
@@ -11,13 +11,13 @@ console.log(hasEven([1,3,5,8]));
 
 function startWithM (myNames: string[]) {
     let i = 0;
-    while (i < myNames.length && myNames[i][i].slice(0) != "M"){
+    while (i < myNames.length && myNames[i].startsWith("M")){
         i++;
     }
     let condicion2 = i < myNames.length;
     return condicion2;
 }
-console.log(startWithM(["Mucía","Lucía"]))
+console.log(startWithM(["Lucía","María"]))
 
 // let palabra: string[] = ["Pepito","Lucia"]
 // console.log(palabra[0][0].slice(0));
